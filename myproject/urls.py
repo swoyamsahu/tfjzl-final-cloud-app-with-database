4a13ad4ca82622f4ad093b38db74a26d8a3f8da8
sahuswoyam <sahuswoyam@gmail.com>
2025-05-04 01:12:54 -0400

    Modified the urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('onlinecourse/', include('onlinecourse.urls')),
-    path('', RedirectView.as_view(url='/onlinecourse/', permanent=False)), 
+    path('', RedirectView.as_view(url='/onlinecourse/', permanent=False)),  
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
